eab45b6d8aa17c4f3035c0d84ddfb882e4f2b12e
Quazi Samiha <quazisamiha@gmail.com>
2023-10-03 10:38:55 -0700

    navbar and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import { router } from "./Routes/Routes";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router} />
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
